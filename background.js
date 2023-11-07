b11b0afc96c3d36cc27a76533e07e501bdb57c4a
Lanceart <lancellq@gmail.com>
2023-11-06 17:54:06 -0800

    step: relations

--- background.js
+++ background.js
@@ -21,7 +21,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     if (currentTabId && details.tabId === currentTabId) {
       // console.log(details);
       // !!! important
-      // console.log(`URL Requested from active tab (${currentTabId}): ${details.url}, Type: ${details.type}, Initiator: ${details.initiator}`);
+      console.log(`URL Requested from active tab (${currentTabId}): ${details.url}, Type: ${details.type}, Initiator: ${details.initiator}`);
       // If you want to communicate with the popup, you can send a message:
       // chrome.runtime.sendMessage({tabId: currentTabId, url: details.url});
     }
@@ -39,11 +39,31 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
 // background.js
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log(message.type);
   if (message.type === 'script-added' || message.type === 'script-loaded') {
-    console.log('Script source:', message.src, 'from page:', message.pageUrl);
+    // !!!important
+    console.log('Type:', message.type,'Script source:', message.src, 'from page:', message.pageUrl);
     // Here, you can keep track of the scripts and their relationships
     // You could, for instance, keep a map of pages to scripts
   }
+
+  if (message.type === 'script-src-set') {
+    console.log('Script source set:', message.src);
+    const stack = message.stack;
+
+    // 这里你需要写一些解析堆栈信息的逻辑
+    // 以下代码只是一个非常简单的例子，实际上你可能需要更复杂的正则表达式
+    const lines = stack.split('\n');
+    lines.forEach((line) => {
+      // 假设我们正在寻找的格式是 "at [function name] ([file url]:[line number]:[column number])"
+      const match = line.match(/at .+ \((.+):(\d+):(\d+)\)/);
+      if (match) {
+        const [, fileUrl, lineNumber, columnNumber] = match;
+        console.log(`Initiator file: ${fileUrl}, line: ${lineNumber}, column: ${columnNumber}`);
+      }
+    });
+  }
+
 });
 
 
